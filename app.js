fa07ffcdfddc984a7c8b5a471a6f616ecbf99369
後閑諒一 <nargok@gmail.com>
2018-01-03 16:53:37 +0900

    Create status change process

--- app.js
+++ app.js
@@ -18,7 +18,19 @@ var filters = {
 
 Vue.component('task-card', {
   props: ['task'],
-  template: '<div class="card"><div class="card-content">{{ task.name }}</div><footer class="card-footer"><div class="card-footer-item">{{ task.assignee }}</div><div class="card-footer-item">{{ task.mandays }} 人日</div></footer><footer class="card-footer"><a class="card-footer-item">◀︎</a><a class="card-footer-item">▶︎</a></footer></div>'
+  template: '<div class="card"><div class="card-content">{{ task.name }}</div><footer class="card-footer"><div class="card-footer-item">{{ task.assignee }}</div><div class="card-footer-item">{{ task.mandays }} 人日</div></footer><footer class="card-footer"><a class="card-footer-item" v-on:click="decrementStatus(task)">◀︎</a><a class="card-footer-item" v-on:click="incrementStatus(task)">▶︎</a></footer></div>',
+  methods: {
+    incrementStatus: function (task) {
+      if(1 <= task.status && task.status <= 2) {
+        task.status++
+      }
+    },
+    decrementStatus: function (task) {
+      if(2 <= task.status && task.status <= 3) {
+        task.status--
+      }
+    }
+  }
 })
 
 new Vue({
