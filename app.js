25fcf1b8b172db443676d2a214830c9ffc2462b8
後閑諒一 <nargok@gmail.com>
2018-01-03 16:17:05 +0900

    Create computed property in app.js

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
+var filters = {
+  open: function (tasks) {
+    return tasks.filter(function (task) {
+      return task.status === 1
+    })
+  },
+  doing: function (tasks) {
+    return tasks.filter(function (task) {
+      return task.status === 2
+    })
+  },
+  closed: function (tasks) {
+    return tasks.filter(function (task) {
+      return task.status === 3
+    })
+  }
+}
+
 new Vue({
   el: '#board',
   data: {
@@ -7,5 +25,19 @@ new Vue({
       { name: 'task3', status: 2, assignee: 'Sabro', mandays: 1 },
       { name: 'task4', status: 3, assignee: 'Shiro', mandays: 1 }
     ]
+  },
+  // 定義した処理を呼ぶ
+  // Vueインスタンスでtasksデータが更新された際に評価される
+  computed: {
+    tasksOpen: function () {
+      return filters.open(this.tasks)
+    },
+    tasksDoing: function () {
+      return filters.doing(this.tasks)
+    },
+    tasksClosed: function () {
+      return filters.closed(this.tasks)
+    }
   }
 })
+
