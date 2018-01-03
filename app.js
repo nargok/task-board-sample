8781d6479e54c48e7fa9edc38e386f6447d8a665
後閑諒一 <nargok@gmail.com>
2018-01-03 16:46:46 +0900

    Create template of task-card

--- app.js
+++ app.js
@@ -16,6 +16,11 @@ var filters = {
   }
 }
 
+Vue.component('task-card', {
+  props: ['task'],
+  template: '<div class="card"><div class="card-content">{{ task.name }}</div><footer class="card-footer"><div class="card-footer-item">{{ task.assignee }}</div><div class="card-footer-item">{{ task.mandays }} 人日</div></footer><footer class="card-footer"><a class="card-footer-item">◀︎</a><a class="card-footer-item">▶︎</a></footer></div>'
+})
+
 new Vue({
   el: '#board',
   data: {
